d8c26c33d8a0a8df8f89ff61667e74dd3ffc3ab2
msdio <rlagofka1@gmail.com>
2024-03-02 17:39:50 +0900

    setting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+  },
 };
