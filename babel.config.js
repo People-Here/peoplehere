25f2185ea98fd026300630ca728f7a9ac7b07b87
msdio <dev.msdio@gmail.com>
2024-03-08 21:15:17 +0900

    config typescript, babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@constants': './src/constants',
+          '@types': './src/types',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
