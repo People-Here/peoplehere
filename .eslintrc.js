4e85f2dfa077eb866d6b5746aacff60925cfe680
msdio <dev.msdio@gmail.com>
2024-03-08 23:28:48 +0900

    add prettier, eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,29 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint', 'unused-imports'],
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'unused-imports'],
   rules: {
     'unused-imports/no-unused-imports': 'error',
+    'no-console': 'warn',
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': ['warn', {before: false, after: true}],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {prefer: 'type-imports'},
+    ],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
+  jsxSingleQuote: true,
   trailingComma: 'all',
   useTabs: false,
   tabWidth: 2,
+  printWidth: 100,
 };
