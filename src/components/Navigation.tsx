1f1794263177f67f923ac3a8d64e337e9556c2fe
msdio <rlagofka1@gmail.com>
2024-03-23 00:02:45 +0900

    clean project

--- src/components/Navigation.tsx
+++
@@ -1,9 +0,0 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
-
-const Navigation = () => {
-  return <Tab.Navigator></Tab.Navigator>;
-};
-
-export default Navigation;
