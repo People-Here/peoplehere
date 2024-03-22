ab947e16b881ebf4217b8a984b02e33aca33a054
msdio <rlagofka1@gmail.com>
2024-03-23 00:00:46 +0900

    navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+  return <Tab.Navigator></Tab.Navigator>;
+};
+
+export default Navigation;
