a9118dbeee15a68f1644461711327912991181c8
msdio <rlagofka1@gmail.com>
2024-03-24 00:01:59 +0900

    feat: design system - colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 import typos from "./src/theme/typos";
+import colors from "./src/theme/colors";
 
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: colors,
   },
   plugins: [
     ({ addUtilities }) => {
