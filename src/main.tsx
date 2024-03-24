af167871af77efa213e9a8065c20ce423ecefd9b
msdio <rlagofka1@gmail.com>
2024-03-24 21:49:51 +0900

    chore: setting eslint, prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -7,5 +8,5 @@ const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/theme/typos.ts
+++ src/theme/typos.ts
@@ -1,90 +1,90 @@
 const typos = {
-  ".font-caption3": {
-    fontSize: ".625rem",
+  '.font-caption3': {
+    fontSize: '.625rem',
     fontWeight: 400,
-    lineHeight: "1rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-caption2": {
-    fontSize: "0.75rem",
+  '.font-caption2': {
+    fontSize: '0.75rem',
     fontWeight: 400,
-    lineHeight: "1.125rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.125rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-caption1": {
-    fontSize: ".75rem",
+  '.font-caption1': {
+    fontSize: '.75rem',
     fontWeight: 600,
-    lineHeight: "1.125rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.125rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-body2": {
-    fontSize: ".875rem",
+  '.font-body2': {
+    fontSize: '.875rem',
     fontWeight: 400,
-    lineHeight: "1.375rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.375rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-body1": {
-    fontSize: ".875rem",
+  '.font-body1': {
+    fontSize: '.875rem',
     fontWeight: 500,
-    lineHeight: "1.375rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.375rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-subheading3": {
-    fontSize: "1rem",
+  '.font-subheading3': {
+    fontSize: '1rem',
     fontWeight: 400,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-subheading2": {
-    fontSize: "1rem",
+  '.font-subheading2': {
+    fontSize: '1rem',
     fontWeight: 500,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-subheading1": {
-    fontSize: "1rem",
+  '.font-subheading1': {
+    fontSize: '1rem',
     fontWeight: 600,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-heading": {
-    fontSize: "1.125rem",
+  '.font-heading': {
+    fontSize: '1.125rem',
     fontWeight: 600,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-title2": {
-    fontSize: "1.25rem",
+  '.font-title2': {
+    fontSize: '1.25rem',
     fontWeight: 500,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-title1": {
-    fontSize: "1.25rem",
+  '.font-title1': {
+    fontSize: '1.25rem',
     fontWeight: 600,
-    lineHeight: "1.5rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.5rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-headline3": {
-    fontFamily: "SUITE Variable",
-    fontSize: "1rem",
+  '.font-headline3': {
+    fontFamily: 'SUITE Variable',
+    fontSize: '1rem',
     fontWeight: 700,
-    lineHeight: "1.875rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.875rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-headline2": {
-    fontFamily: "SUITE Variable",
-    fontSize: "1.125rem",
+  '.font-headline2': {
+    fontFamily: 'SUITE Variable',
+    fontSize: '1.125rem',
     fontWeight: 700,
-    lineHeight: "1.875rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.875rem',
+    letterSpacing: '-0.0125rem',
   },
-  ".font-headline1": {
-    fontFamily: "SUITE Variable",
-    fontSize: "1.25rem",
+  '.font-headline1': {
+    fontFamily: 'SUITE Variable',
+    fontSize: '1.25rem',
     fontWeight: 700,
-    lineHeight: "1.875rem",
-    letterSpacing: "-0.0125rem",
+    lineHeight: '1.875rem',
+    letterSpacing: '-0.0125rem',
   },
 };
 
