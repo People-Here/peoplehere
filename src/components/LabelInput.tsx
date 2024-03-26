e29f79381e11a3b22ea716f17c24a678939665dc
msdio <rlagofka1@gmail.com>
2024-03-26 22:53:11 +0900

    create labeled input:

--- src/components/LabelInput.tsx
+++ src/components/LabelInput.tsx
@@ -3,14 +3,30 @@ import { IonInput, IonItem, IonLabel } from '@ionic/react';
 type Props = {
   label: string;
   value: string;
-  onChange: (value: string) => void;
+  onChange: (input: string) => void;
 };
 
 const LabelInput = ({ label, value, onChange }: Props) => {
   return (
-    <IonItem>
-      <IonLabel position="floating">{label}</IonLabel>
-      <IonInput value={value} onIonInput={(event) => onChange(event.target.value as string)} />
+    <IonItem
+      className="h-[54px]"
+      style={{
+        '--background': '#F4F5F7',
+        '--border-radius': '12px',
+        '--border-width': 0,
+      }}
+      lines="none"
+    >
+      <IonInput
+        className="font-body1 text-gray8"
+        label={label}
+        labelPlacement="floating"
+        value={value}
+        onIonChange={(e) => onChange(e.target.value as string)}
+        style={{
+          '--highlight-color-focused': '#9FA4A9',
+        }}
+      />
     </IonItem>
   );
 };
