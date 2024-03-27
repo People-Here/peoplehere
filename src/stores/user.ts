d53e86f07380f95e2c2422b12fb8e46d3d76e965
msdio <rlagofka1@gmail.com>
2024-03-28 00:45:48 +0900

    feat: persist zustand storage value

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,16 +1,29 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type State = {
-  region: string;
+  region: {
+    '2digitCode': string;
+    ISONumbericCode: number;
+  };
 };
 
 type Action = {
   setRegion: (region: State['region']) => void;
 };
 
-const useRegionStore = create<State & Action>((set) => ({
-  region: '',
-  setRegion: (region: string) => set(() => ({ region })),
-}));
+const useRegionStore = create(
+  persist<State & Action>(
+    (set) => ({
+      region: {
+        '2digitCode': '',
+        ISONumbericCode: 0,
+      },
+
+      setRegion: (region: State['region']) => set(() => ({ region })),
+    }),
+    { name: 'region-storage' },
+  ),
+);
 
 export default useRegionStore;
