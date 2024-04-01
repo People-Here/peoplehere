644d792fd739c4d52844ce8b37ef3d9a33a3aca6
msdio <dev.msdio@gmail.com>
2024-04-01 17:14:27 +0900

    feat: create api instances

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const apiInstance = axios.create({
+  baseURL: '',
+  withCredentials: true,
+  responseType: 'json',
+});
+
+export const typedGet = async <T>(url: string, config?: AxiosRequestConfig) => {
+  const response = await apiInstance.get<AxiosResponse<T>>(url, config);
+  return response.data;
+};
+
+export const typedPost = async <T>(url: string, body?: unknown, config?: AxiosRequestConfig) => {
+  const response = await apiInstance.post<AxiosResponse<T>>(url, body, config);
+  return response.data;
+};
+
+export const typedPatch = async <T>(url: string, body?: unknown, config?: AxiosRequestConfig) => {
+  const response = await apiInstance.patch<AxiosResponse<T>>(url, body, config);
+  return response.data;
+};
