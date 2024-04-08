790ffe10dc496ea1f109301baec9945c2f70615f
msdio <rlagofka1@gmail.com>
2024-04-09 01:05:43 +0900

    feat: connecting api with search place

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -2,16 +2,28 @@ import { IonInput, IonItem, IonLabel, IonList, IonText } from '@ionic/react';
 import { useState } from 'react';
 
 import Header from '../../components/Header';
+import { searchTour } from '../../api/tour';
+
+import type { Tour } from '../../api/tour';
 
 const Search = () => {
   const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState<Tour[]>([]);
+
+  const onSearch = async () => {
+    const response = await searchTour(search);
+
+    if (response.status === 200) {
+      setSearchResult(response.data.tourList);
+    }
+  };
 
   return (
     <>
       <Header type="back" />
 
       <div className="px-4 mt-2.5">
-        <SearchBar search={search} setSearch={setSearch} />
+        <SearchBar search={search} setSearch={setSearch} onSearch={onSearch} />
 
         {search.length === 0 ? (
           <div className="mt-5">
@@ -34,9 +46,10 @@ const Search = () => {
 type SearchBarProps = {
   search: string;
   setSearch: (value: string) => void;
+  onSearch: () => void;
 };
 
-const SearchBar = ({ search, setSearch }: SearchBarProps) => {
+const SearchBar = ({ search, setSearch, onSearch }: SearchBarProps) => {
   return (
     <div className="border border-gray3 rounded-lg bg-gray1.5 px-4 flex items-center">
       <IonInput
@@ -45,11 +58,20 @@ const SearchBar = ({ search, setSearch }: SearchBarProps) => {
         clearInput
         value={search}
         onIonInput={(e) => setSearch(e.detail.value as string)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onSearch();
+          }
+        }}
       />
     </div>
   );
 };
 
+type SearchListProps = {
+  id: string;
+  title: string;
+};
 const SearchList = () => {
   return (
     <IonList lines="none">
