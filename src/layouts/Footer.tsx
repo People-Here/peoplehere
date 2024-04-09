8cb1ed8a717ed2df764240e07626fd0956a66d25
msdio <dev.msdio@gmail.com>
2024-04-09 13:13:24 +0900

    fix: add background color on footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ import type { PropsWithChildren } from 'react';
 
 const Footer = ({ children }: PropsWithChildren) => {
   return (
-    <IonFooter class="ion-no-border" className="fixed bottom-0 left-0 right-0">
+    <IonFooter class="ion-no-border" className="fixed bottom-0 left-0 right-0 bg-white">
       <IonToolbar className="p-4">{children}</IonToolbar>
     </IonFooter>
   );
