b93e28d8b86f194eab74099a054f2bb4d37dae04
HR <rlagofka1@gmail.com>
2024-04-09 15:15:52 +0900

    투어 검색 기능 수정 (#17)
    * fix: search response type
    
    * feat: 준비중인 기능입니다 modal on pages not completed yet
    
    * fix: 장소 만들기에서 login 페이지로 갈 경우 replace로 이동하도록 변경:

--- src/tabs/BookmarkTab.tsx
+++ src/tabs/BookmarkTab.tsx
@@ -1,6 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { useEffect, useRef } from 'react';
+
+import Alert from '../components/alerts';
 
 const BookmarkTab = () => {
+  const router = useIonRouter();
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    buttonRef.current?.click();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -15,6 +26,16 @@ const BookmarkTab = () => {
           </IonToolbar>
         </IonHeader>
       </IonContent>
+
+      <button id="ready-alert" ref={buttonRef} className="hidden" />
+
+      <Alert
+        trigger="ready-alert"
+        title="준비중인 기능이에요."
+        buttons={[
+          { text: '홈으로 돌아가기', onClick: () => router.push('/', 'forward', 'replace') },
+        ]}
+      />
     </IonPage>
   );
 };
