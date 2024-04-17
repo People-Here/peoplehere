bc77b6994efdc032ab8a813d42af455e0e0bc6a5
msdio <rlagofka1@gmail.com>
2024-04-18 00:19:26 +0900

    feat: create simple input modal

---
+++ src/modals/SimpleInputModal.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { IonText } from '@ionic/react';
+
+import ModalContainer from '.';
+
+import type { ModalProps } from '.';
+
+type Props = {
+  title: string;
+  placeholder: string;
+  maxLength: number;
+  setValue: (value: string) => void;
+};
+
+const SimpleInputModal = ({
+  title,
+  placeholder,
+  maxLength,
+  setValue,
+  ...props
+}: Props & ModalProps) => {
+  const [input, setInput] = useState('');
+
+  return (
+    <ModalContainer
+      title={title}
+      buttonText="저장"
+      initialBreakpoint={0.3}
+      breakpoints={[0, 0.3, 0.5]}
+      onClickButton={() => setValue(input)}
+      {...props}
+    >
+      <div className="flex flex-col gap-1.5 items-end">
+        <input
+          className="px-4 py-2.5 rounded-lg bg-gray1.5 font-body1 text-gray5"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+          placeholder={placeholder}
+          maxLength={maxLength}
+        />
+
+        <IonText className="font-caption2 text-gray5">
+          {input.length}/{maxLength}
+        </IonText>
+      </div>
+    </ModalContainer>
+  );
+};
+
+export default SimpleInputModal;
