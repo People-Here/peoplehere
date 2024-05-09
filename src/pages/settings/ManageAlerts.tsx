7d6bc97a53137abc0bde5d5e062ec814b093c155
msdio <dev.msdio@gmail.com>
2024-05-09 17:31:47 +0900

    feat: change alarm consent

--- src/pages/settings/ManageAlerts.tsx
+++ src/pages/settings/ManageAlerts.tsx
@@ -2,6 +2,11 @@ import { IonContent, IonPage } from '@ionic/react';
 import { useState } from 'react';
 
 import Header from '../../components/Header';
+import { updateAlarmStatus } from '../../api/alarm';
+import { getNewToken } from '../../api/login';
+
+import type { AxiosError } from 'axios';
+import type { AlarmType } from '../../api/alarm';
 
 const ManageAlerts = () => {
   return (
@@ -18,12 +23,14 @@ const ManageAlerts = () => {
           <ListItem
             title="새로운 쪽지 알림"
             content="다른 사용자가 보낸 쪽지에 대한 알림을 받습니다."
-            onOff={false}
+            onOff={true}
+            type="MESSAGE"
           />
           <ListItem
             title="마케팅 정보 수신"
             content="피플히어의 업데이트 소식, 이벤트, 프로모션 등 마케팅과 관련된 알림을 받습니다."
             onOff={true}
+            type="MARKETING"
           />
         </div>
       </IonContent>
@@ -35,8 +42,26 @@ type ListItemProps = {
   title: string;
   content: string;
   onOff: boolean;
+  type: AlarmType;
 };
-const ListItem = ({ title, content, onOff }: ListItemProps) => {
+const ListItem = ({ title, content, onOff, type }: ListItemProps) => {
+  const [isOn, setIsOn] = useState(onOff);
+
+  const updateAlert = async (type: AlarmType, onOff: boolean) => {
+    try {
+      await updateAlarmStatus({ type, consent: onOff });
+    } catch (error) {
+      const errorInstance = error as AxiosError;
+
+      if (errorInstance.response?.status === 401) {
+        await getNewToken();
+        await updateAlarmStatus({ type, consent: onOff });
+      }
+
+      console.error('fail to update alarm', error);
+    }
+  };
+
   return (
     <div className="flex gap-4 justify-between py-5 border-b border-gray1.5 w-full">
       <div>
@@ -44,26 +69,30 @@ const ListItem = ({ title, content, onOff }: ListItemProps) => {
         <p className="font-caption2 text-gray5.5">{content}</p>
       </div>
 
-      <Switch onOff={onOff} />
+      <Switch
+        onOff={isOn}
+        onChange={async () => {
+          await updateAlert(type, !isOn);
+          setIsOn((prev) => !prev);
+        }}
+      />
     </div>
   );
 };
 
-const Switch = ({ onOff }: { onOff: boolean }) => {
-  const [isOn, setIsOn] = useState(onOff);
-
+const Switch = ({ onOff, onChange }: { onOff: boolean; onChange: () => void }) => {
   return (
     <div
       className={
-        isOn
+        onOff
           ? 'relative w-12 px-0.5 rounded-full h-7 shrink-0 bg-orange5 transition-all duration-300'
           : 'relative w-12 px-0.5 rounded-full h-7 shrink-0 bg-gray5 transition-all duration-300'
       }
-      onClick={() => setIsOn((prev) => !prev)}
+      onClick={onChange}
     >
       <span
         className="absolute w-6 transition-all duration-300 ease-in-out bg-white rounded-full aspect-square top-1/2"
-        style={isOn ? { transform: 'translate(20px, -50%)' } : { transform: 'translateY(-50%)' }}
+        style={onOff ? { transform: 'translate(20px, -50%)' } : { transform: 'translateY(-50%)' }}
       />
       {/* <span className="absolute w-6 transition-all duration-300 ease-in-out translate-x-full -translate-y-1/2 bg-white rounded-full aspect-square top-1/2" /> */}
     </div>
