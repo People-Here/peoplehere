af664b5f646051cfe11b7ce486f02d51d0ef2780
msdio <rlagofka1@gmail.com>
2024-06-02 11:24:25 +0900

    fix: change loading text while posting tour

--- src/pages/post/Preview.tsx
+++ src/pages/post/Preview.tsx
@@ -309,7 +309,7 @@ const SelectTheme = ({ currentTheme, setTheme, onClick, buttonDisable }: ThemePr
           onClick={onClick}
           disabled={buttonDisable}
         >
-          {buttonDisable ? 'loading...' : t('newTour.post')}
+          {buttonDisable ? t('newTour.postLoading') : t('newTour.post')}
         </button>
       </div>
     </IonFooter>
