7d0a4d8520ffc32434debca0032a3bb391fa01e8
HR <rlagofka1@gmail.com>
2024-07-15 00:24:19 +0900

    fcm을 이용한 알림 호출 기능 api (#72)
    * chore: install capacitor push notifications
    
    * feat: setting push notification for ios
    
    * feat: setting push notification for android
    
    * feat: check permissions and notifications
    
    * feat: send notification info when start app
    
    * chore: increase google version code

---
+++ src/api/notification.ts
@@ -0,0 +1,24 @@
+import { Preferences } from '@capacitor/preferences';
+
+import { typedPost } from '.';
+
+export const sendNotification = async () => {
+  const fcmToken = await Preferences.get({ key: 'fcmToken' });
+  const deviceId = await Preferences.get({ key: 'DeviceId' });
+  const accessToken = await Preferences.get({ key: 'accessToken' });
+
+  const response = await typedPost(
+    '/api/notify/token',
+    {
+      token: fcmToken.value,
+      deviceId: deviceId.value,
+    },
+    {
+      headers: {
+        Authorization: accessToken.value,
+      },
+    },
+  );
+
+  return response;
+};
---
+++ src/services/fcmService.ts
@@ -0,0 +1,44 @@
+import { Preferences } from '@capacitor/preferences';
+import { PushNotifications } from '@capacitor/push-notifications';
+
+export const addFCMLogListeners = async () => {
+  await PushNotifications.addListener('registration', async (token) => {
+    await Preferences.set({ key: 'fcmToken', value: token.value });
+    console.log('Push registration success, token: ' + token.value);
+  });
+
+  await PushNotifications.addListener('registrationError', (err) => {
+    console.error('Registration error: ', err.error);
+  });
+
+  await PushNotifications.addListener('pushNotificationReceived', (notification) => {
+    console.log('Push notification received: ', notification);
+  });
+
+  await PushNotifications.addListener('pushNotificationActionPerformed', (notification) => {
+    console.log(
+      'Push notification action performed',
+      notification.actionId,
+      notification.inputValue,
+    );
+  });
+};
+
+export const registerNotifications = async () => {
+  let permission = await PushNotifications.checkPermissions();
+
+  if (permission.receive === 'prompt') {
+    permission = await PushNotifications.requestPermissions();
+  }
+
+  if (permission.receive !== 'granted') {
+    throw new Error('User denied permissions');
+  }
+
+  await PushNotifications.register();
+};
+
+export const getDeliveredNotifications = async () => {
+  const notificationList = await PushNotifications.getDeliveredNotifications();
+  console.log('Delivered notifications: ', notificationList);
+};
